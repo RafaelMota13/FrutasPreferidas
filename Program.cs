ecb8acefb878c775f067a85738a4252c0ae0776b
Rafael Mota <faelmota7@gmail.com>
2022-09-03 20:25:51 +0000

    Resolução

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("--- Minhas frutas preferidas ---");
+Console.WriteLine("1. Abacate");
+Console.WriteLine("2. Goiaba");
+Console.WriteLine("3. Uva");
+Console.WriteLine("4. Kiwi");
+Console.WriteLine("5. Morango");
